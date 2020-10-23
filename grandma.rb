ac035073e6122eaf33550a3232f56b98c0b3c140
stacy <stacydn23@gmail.com>
2020-10-22 20:05:42 -0400

    commit tree 8c2055ad198e435fdac40ae1e5cf81d75f010745

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,8 @@
 
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
-# unless you shout it (type in all capitals).
+
+# unless you shout it (type in all capitals)
 
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
@@ -11,3 +12,19 @@
 
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
+
+def speak_to_grandma(phrase)
+    if phrase.upcase != phrase
+        return "HUH?! SPEAK UP, SONNY!"
+    elsif phrase == "I LOVE YOU GRANDMA!"
+        return "I LOVE YOU TOO PUMPKIN!"
+    else
+        return "NO, NOT SINCE 1938!"
+    end
+end
+
+
+        
+
+
+
